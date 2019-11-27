07158553cfcff0e4a75a85e7e3064687641cc14c
Anmol <anmollp@outlook.com>
2019-11-27 15:33:42 +0530

    Initial Project Add

---
+++ src/main/java/com/anmol/alppetclinic/AlpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.anmol.alppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anmol/alppetclinic/AlpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol.alppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
