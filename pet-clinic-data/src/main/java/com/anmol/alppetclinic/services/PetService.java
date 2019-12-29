aeaf2e20b5b220764afc2bb8e69669d0f5d04be8
Anmol <anmol.patil@lendingkart.com>
2019-12-29 18:12:25 +0530

    Refactoring to common crud interface. Closes #12

--- pet-clinic-data/src/main/java/com/anmol/alppetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/anmol/alppetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package com.anmol.alppetclinic.services;
 
 import com.anmol.alppetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
